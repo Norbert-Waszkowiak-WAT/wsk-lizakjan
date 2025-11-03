224337fc9f79f42efac36bcf401d11d30183d30d
lizakjan <lizakjan@wp.pl>
2025-11-03 09:28:53 +0000

    rozwiązanie zadania 1

---
+++ ex01.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x = 9;
+    int* p = &x;
+    cout << p << endl;
+    cout << *p << endl;
+    *p = 12;
+    cout << x << endl;
+    return 0; 
+}
